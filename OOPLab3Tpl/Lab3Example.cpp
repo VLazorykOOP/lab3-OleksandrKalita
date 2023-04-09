a3d2076af4bcf23e7993d79446b6dd0e8e0df000
Oleksandr Kalita <olexadr.kalita@gmail.com>
2023-04-09 13:50:53 +0200

    Update Lab3Example.cpp
    доповнюємо класс cirlce

--- OOPLab3Tpl/Lab3Example.cpp
+++ OOPLab3Tpl/Lab3Example.cpp
@@ -20,6 +20,34 @@ public:
         radius = valueRadius;
         color = valueColor;
     }
+	int S(){
+        return 3.14 * pow(radius, 2);
+    }
+    int C(){
+        return 2 * 3.14 * radius;
+    }
+    void SetCircleInfo(){
+        int radius;
+        cout << "Input circle radius: ";
+        cin >> radius; 
+        if( radius<0 || radius > 1.e+100) {
+            cout<<" Error set a \n";
+        } else {
+            this->radius = radius;
+        }
+        cout << "Input cirlce color: ";
+        cin >> color;
+
+        if( color<0 || color > 10000){ 
+            cout<<" Error set color \n";
+        } else{
+            this->color = color;
+        }
+    }
+    void ShowInfo(){
+        cout << "Radius: " << radius << endl;
+        cout << "Color: " << color << endl;
+    }
 };
 
 
